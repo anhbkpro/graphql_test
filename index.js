0077da42b2c26351517d95c2f35a8a56c4e9aedb
AnhLai <laingocanh1990@gmail.com>
2021-04-22 20:18:46 +0700

    run the very first graphql query using GraphiQL

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import express from 'express'
+import express from 'express';
+import schema from './schema';
+import {graphqlHTTP} from 'express-graphql';
+
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Graphql is amazing!');
 });
+
+const root = { hello: () => "Hi, I'm Lai Anh" };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8080, () => console.log('Running on server localhost:8080/graphql'));
